2ffef4a80489d072b9d6e96564a31474b7f130a7
yerinko <koyerin956@gmail.com>
2022-07-04 23:09:30 +0900

    next.js 초기 셋팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { useEffect } from "react";
+import { Reset } from "styled-reset";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    if (window.location.hostname.indexOf("yerinko.id") < 0) return;
+  }, []);
+
+  return (
+    <>
+      <Reset />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,81 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          {/* next.js font optimization 은 IE의 User Agent 로 요청한 응답도
+           가져오기 때문에 (getFontDefinitionFromNetwork) 전체 폰트를 다운로드하는 문제가 있음. */}
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;500&display=swap"
+            as="style"
+          />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `</style>
+                        <link
+                          rel="stylesheet"
+                          href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;500&display=swap"
+                          media="print"
+                          onload="this.media='all';"
+                        />
+                        <style>`,
+            }}
+          ></style>
+          <noscript>
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;500&display=swap"
+            />
+          </noscript>
+
+          {/* Google Analytics */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
