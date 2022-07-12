cf589fe15458f55afc5d7965b925075e43afb751
yerinko <koyerin956@gmail.com>
2022-07-12 16:08:50 +0900

    사진 확대 시 필요로하는 Modal 컴포넌트 제작

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const ModalWrap = styled.div`
+  display: flex;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  flex-direction: column;
+  justify-content: center;
+  overflow: auto;
+  backdrop-filter: blur(10px);
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  width: 100%;
+  mac-width: 600px;
+  margin: 0 auto;
+`;
+
+type Props = {
+  children: React.ReactNode;
+  handleClose: () => void;
+};
+const Modal = ({ children, handleClose }: Props) => {
+  return (
+    <ModalWrap onClick={handleClose}>
+      <ModalContent>{children}</ModalContent>
+    </ModalWrap>
+  );
+};
+
+export default Modal;
